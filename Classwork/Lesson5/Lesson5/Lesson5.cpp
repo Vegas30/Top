989f4880b7c97f972a8ee3817d4abba6efef5ee0
Igor Valuiskov <i_valuiskov@mail.ru>
2023-06-22 21:19:04 +0400

    добавлен калькулятов в Lesson5

--- Classwork/Lesson5/Lesson5/Lesson5.cpp
+++ Classwork/Lesson5/Lesson5/Lesson5.cpp
@@ -1,12 +1,38 @@
-﻿// Lesson5.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
 
-#include <iostream>
+using namespace std;
 
 int main()
 {
+	setlocale(LC_ALL, "Rus");
 
-    std::cout << "Hello World!\n";
+	double a, b;
+	string x;
+
+	cout << "Введите первое число: ";
+	cin >> a;
+
+	cout << "Выберите вид операции: \n+\n-\n*\n/ ";
+	cin >> x;
+	cout << "Введите второе число: ";
+	cin >> b;
+
+	if (x == "+")
+	{
+		cout << "Результат: " << a + b << endl;
+	}
+	else if (x == "-")
+	{
+		cout << "Результат: " << a - b << endl;
+	}
+	else if (x == "/")
+	{
+		cout << "Результат: " << a / b << endl;
+	}
+	else 
+	{
+		cout << "Результат: " << a * b << endl;
+	}
 
 }
 
