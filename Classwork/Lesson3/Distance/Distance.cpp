de928f3bf2e07c96ec13a094f160cc77c2eed20b
Igor Valuiskov <i_valuiskov@mail.ru>
2023-06-27 15:16:30 +0400

    Добавлена задача Distance

--- Classwork/Lesson3/Distance/Distance.cpp
+++ Classwork/Lesson3/Distance/Distance.cpp
@@ -1,28 +1,31 @@
 ﻿#include <iostream>
+#include <iomanip>
 
 using namespace std;
 
 int main()
 {
-    setlocale(LC_ALL, "Rus");
+	setlocale(LC_ALL, "Rus");
 
-    int distance_in_feet;
-    const int inch_in_feet = 12;
-    const int feet_in_yard = 3;
-    const int feet_in_miles = 5280 ;
-    double dist_in_inches, dist_in_yards, dist_in_miles;
-    cout << "Введите расстояние в футах: ";
-    cin >> distance_in_feet;
+	const int inch_in_feet = 12;
+	const int feet_in_yard = 3;
+	const int feet_in_miles = 5280;
+	double distance_in_feet, dist_in_inches, dist_in_yards, dist_in_miles;
 
-    dist_in_inches = distance_in_feet * inch_in_feet;
+	cout << "Введите расстояние в футах: ";
+	cin >> distance_in_feet;
 
-
-    std::cout << "Расстояние в дюймах: " << distance_in_feet * inch_in_feet << " дюймов" << std::endl;
-    std::cout << "Расстояние в ярдах: " << distance_in_feet * << " ярдов" << std::endl;
-    std::cout << "Расстояние в милях: " << distance_in_feet << " миль" << std::endl;
+	dist_in_inches = distance_in_feet * inch_in_feet;
+	dist_in_yards = distance_in_feet / feet_in_yard;
+	dist_in_miles = distance_in_feet / feet_in_miles;
 
 
-    return 0;
+	cout << "Расстояние в дюймах: " << fixed << setprecision(3) << dist_in_inches << " дюймов" << endl;
+	cout << "Расстояние в ярдах: " <<  fixed << setprecision(3) << dist_in_yards << " ярдов" << endl;
+	cout << "Расстояние в милях: " << fixed << setprecision(3) << dist_in_miles << " миль" << endl;
+
+
+	return 0;
 
 
 
