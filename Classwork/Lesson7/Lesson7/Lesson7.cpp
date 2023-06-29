8c8bbf3b82c3735e00b85654b8c15a82e5733142
Igor Valuiskov <i_valuiskov@mail.ru>
2023-06-29 19:10:41 +0400

    добавлен Lesson7

--- Classwork/Lesson7/Lesson7/Lesson7.cpp
+++ Classwork/Lesson7/Lesson7/Lesson7.cpp
@@ -5,6 +5,16 @@ using namespace std;
 int main()
 {
     setlocale(LC_ALL, "Rus");
+    int d;
+    cout << "Введите кол-во денег: ";
+    cin >> d;
+    int p = 2;
+    int s = 0;
 
+    while (d > 0) {
+        d -= p;
+        s++;
+    }
+    cout << "Можно купить " << s << " мороженых" << endl;
 
 }
