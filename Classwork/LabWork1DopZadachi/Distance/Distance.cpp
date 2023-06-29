81dbf0d827049a89ebbaa6118f1deef9a3ed0864
Igor Valuiskov <i_valuiskov@mail.ru>
2023-06-29 21:21:35 +0400

    Добавленны задачи в LabWork1DopZadachi

---
+++ Classwork/LabWork1DopZadachi/Distance/Distance.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+
+	const int inch_in_feet = 12;
+	const int feet_in_yard = 3;
+	const int feet_in_miles = 5280;
+	double distance_in_feet, dist_in_inches, dist_in_yards, dist_in_miles;
+
+	cout << "Введите расстояние в футах: ";
+	cin >> distance_in_feet;
+
+	dist_in_inches = distance_in_feet * inch_in_feet;
+	dist_in_yards = distance_in_feet / feet_in_yard;
+	dist_in_miles = distance_in_feet / feet_in_miles;
+
+
+	cout << "Расстояние в дюймах: " << fixed << setprecision(3) << dist_in_inches << " дюймов" << endl;
+	cout << "Расстояние в ярдах: " << fixed << setprecision(3) << dist_in_yards << " ярдов" << endl;
+	cout << "Расстояние в милях: " << fixed << setprecision(3) << dist_in_miles << " миль" << endl;
+
+
+	return 0;
+
+
+
+
+}
---
+++ Classwork/LabWork1DopZadachi/UnitsOfTimeAgain/UnitsOfTimeAgain.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <format>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+
+	int days, hours, minutes, seconds;
+	cout << "Введите количество секунд: ";
+	cin >> seconds;
+
+	days = seconds / 86400;
+	seconds -= days*86400;
+	hours = seconds / 3600;
+	seconds -= hours * 3600;
+	minutes = seconds / 60;
+	seconds -= minutes * 60;
+
+	cout << format("{:02}:{:02}:{:02}:{:02}", days, hours, minutes, seconds) << endl;
+
+}
