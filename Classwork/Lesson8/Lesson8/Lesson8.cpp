93669628391942b954dd893e46906bfaa2da7942
Igor Valuiskov <i_valuiskov@mail.ru>
2023-06-30 20:30:27 +0400

    добавлены задачи

--- Classwork/Lesson8/Lesson8/Lesson8.cpp
+++ Classwork/Lesson8/Lesson8/Lesson8.cpp
@@ -4,12 +4,48 @@ using namespace std;
 
 int main()
 {
-    setlocale(LC_ALL, "Rus");
+	setlocale(LC_ALL, "Rus");
 
-    int count;
-    for (count = 0; count <= 5; count++)
-    {
-        cout << "Число: " << count << endl;
-    }
+	// вечный цикл
+	//int count;
+	//for (count = 1; count > 0; count++)
+	//{
+	//    cout << "Число: " << count << endl;
+	//}
+
+
+	//for (int count = 1; count < 10; count++)
+	//{
+	//	if (count == 5)
+	//	{
+	//		break; // на 5 выходим
+	//	}
+	//	cout << "Число: " << count << endl;
+
+	//}
+
+
+	//for (int count = 1; count < 10; count++)
+	//{
+	//	if (count == 5)
+	//	{
+	//		continue; // пропускаем 5
+	//	}
+	//	    cout << "Число: " << count << endl;
+
+	//}
+
+	int don = 0;
+	int	hour = 0;
+
+	for (int count = 0; count < 12; count++)
+	{
+		hour++;
+		don += hour;
+
+		cout << "Час: " << hour << endl;
+
+	}
+	cout << "Пробьют: " << don << endl;
 
 }
