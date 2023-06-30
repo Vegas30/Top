b2065793d5394f95e53d1f3502c715b2d80b9d1a
Igor Valuiskov <i_valuiskov@mail.ru>
2023-06-30 18:53:19 +0400

    добавлен урок Lesson8

---
+++ Classwork/Lesson8/Lesson8/Lesson8.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+
+    int count;
+    for (count = 0; count <= 5; count++)
+    {
+        cout << "Число: " << count << endl;
+    }
+
+}
