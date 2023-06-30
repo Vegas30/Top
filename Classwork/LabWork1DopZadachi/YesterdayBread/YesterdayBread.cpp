9e08480f509d3d17f1da27fd0589217e44cae4d7
Igor Valuiskov <i_valuiskov@mail.ru>
2023-06-30 16:27:49 +0400

    закончена последняя задача Вчерашний хлеб

--- Classwork/LabWork1DopZadachi/YesterdayBread/YesterdayBread.cpp
+++ Classwork/LabWork1DopZadachi/YesterdayBread/YesterdayBread.cpp
@@ -1,15 +1,25 @@
 ﻿#include <iostream>
+#include <iomanip>
 
 using namespace std;
 
-int main()
-{
-	setlocale(LC_ALL, "Rus");
-	
-	double bread_price = 3.49;
-	int discount = 60;
-	int bread_amount;
-	cout << "Введите количество вчерашнего хлеба";
+int main() {
 
+    setlocale(LC_ALL, "Rus");
 
+    double regularPrice = 3.49;  // Обычная цена за буханку хлеба
+    double discount = 0.60;  // Скидка на вчерашний хлеб (60%)
+    int quantity;
+
+    cout << "Введите количество приобретенных вчерашних буханок хлеба: ";
+    cin >> quantity;
+
+    double discountedPrice = regularPrice * (1 - discount);  // Цена со скидкой
+    double totalCost = quantity * discountedPrice;  // Общая стоимость
+
+    cout << "Обычная цена за буханку: $" << fixed << setprecision(2) << regularPrice << endl;
+    cout << "Цена со скидкой: $" << fixed << setprecision(2) << discountedPrice << endl;
+    cout << "Общая стоимость приобретенного хлеба: $" << fixed << setprecision(2) << totalCost << endl;
+
+    return 0;
 }
