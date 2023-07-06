c81b0a4c5bf38eb100f249fc7dc936703cce8243
Igor Valuiskov <i_valuiskov@mail.ru>
2023-07-06 21:21:46 +0400

    добывлены задачки

---
+++ Classwork/LabWork2/Presidents/Presidents.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+
+
+	int nominal;
+	while (true)
+	{
+		cout << "Выход: 0\n";
+		cout << "Введите номинал банкноты: ";
+		cin >> nominal;
+
+		switch (nominal)
+		{
+		case 1:
+			cout << "Джордж Вашингтон\n";
+			break;
+		case 2:
+			cout << "Томас Джефферсон\n";
+			break;
+		case 5:
+			cout << "Авраам Линкольн\n";
+			break;
+		case 10:
+			cout << "Александр Гамильтон\n";
+			break;
+		case 20:
+			cout << "Эндрю Джексон\n";
+			break;
+		case 50:
+			cout << "Улисс Грант\n";
+			break;
+		case 100:
+			cout << "Бенжамин Франклин\n";
+			break;
+		case 0:
+			cout << "Выход\n";
+			return 0;
+		default:
+			cout << "Введеного номинала не существует, повторите попытку. ";
+			break;
+		}
+	}
+}
