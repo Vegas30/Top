a0e162f84d2ee9e82003597cef125032479b9ec5
Igor Valuiskov <i_valuiskov@mail.ru>
2023-07-21 16:37:00 +0400

    добавлены задачи Classwork Zadachi

---
+++ Classwork/FindMinMaxInArray/FindMinMaxInArray.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+
+void findMinMax(int arr[], int size, int& minValue, int& minIndex, int& maxValue, int& maxIndex) {
+    if (size <= 0) {
+        std::cout << "Ошибка: передан пустой массив.\n";
+        return;
+    }
+
+    minValue = arr[0];
+    minIndex = 0;
+    maxValue = arr[0];
+    maxIndex = 0;
+
+    for (int i = 1; i < size; ++i) {
+        if (arr[i] < minValue) {
+            minValue = arr[i];
+            minIndex = i;
+        }
+
+        if (arr[i] > maxValue) {
+            maxValue = arr[i];
+            maxIndex = i;
+        }
+    }
+}
+
+int main() {
+    int size;
+    std::cout << "Введите размер массива: ";
+    std::cin >> size;
+
+    if (size <= 0) {
+        std::cout << "Ошибка: недопустимый размер массива.\n";
+        return 1;
+    }
+
+    int* arr = new int[size];
+    std::cout << "Введите элементы массива: ";
+    for (int i = 0; i < size; ++i) {
+        std::cin >> arr[i];
+    }
+
+    int minValue, minIndex, maxValue, maxIndex;
+    findMinMax(arr, size, minValue, minIndex, maxValue, maxIndex);
+
+    std::cout << "Минимальное значение: " << minValue << ", его индекс: " << minIndex << std::endl;
+    std::cout << "Максимальное значение: " << maxValue << ", его индекс: " << maxIndex << std::endl;
+
+    delete[] arr;
+    return 0;
+}
--- Classwork/Zadachi/Zadachi.cpp
+++ Classwork/Zadachi/Zadachi.cpp
@@ -4,23 +4,29 @@
 using namespace std;
 
 
-void pramougolnik()
+void pramougolnik(int height, int width)
 {
-	int n, k;
-	cout << "Введите высоту: ";
-	cin >> n;
-	cout << "Введите ширину: ";
-	cin >> k;
-	
-	for (int i = 1; i <= k; i++)
+
+	for (int i = 0; i < height; ++i) 
 	{
-		for (int j = 0; j < n; j++)
+		for (int j = 0; j < width; ++j) 
 		{
-			cout << "*";
+			if (i == 0 || i == height - 1 || j == 0 || j == width - 1) 
+			{
+				cout << "-";
+			}
+			else 
+			{
+				cout << " ";
+			}
 		}
-		cout << "\n";
+		cout << endl;
 	}
 
+}
+
+void rectangle(int height, int weidth)
+{
 
 }
 
@@ -38,15 +44,76 @@ unsigned long long factorial(int n)
 	}
 }
 
+bool isPrime(int num) {
+	if (num <= 1) {
+		return false; // Число меньше или равно 1, не является простым
+	}
+
+	// Проверяем делители от 2 до sqrt(num)
+	for (int i = 2; i * i <= num; ++i) {
+		if (num % i == 0) {
+			return false; // Найден делитель, число не является простым
+		}
+	}
+
+	return true; // Нет делителей, число является простым
+}
+
+int cube(int number) {
+	return number * number * number;
+}
+
 
 int main()
 {
 	setlocale(LC_ALL, "Rus");
-	 
-	pramougolnik();
+	
+	int height, width, n, num, num_cube;
+
+	// Запрашиваем у пользователя высоту и ширину прямоугольника
+	cout << "Введите высоту прямоугольника: ";
+	cin >> height;
+
+	cout << "Введите ширину прямоугольника: ";
+	cin >> width;
+
+	// Выводим прямоугольник на экран
+	pramougolnik(height, width);
 
 
-	unsigned long long res = factorial(n);
+
 	cout << "Введите число: ";
-		cin >> 
+	cin >> n;
+
+	if (n >= 0) {
+		unsigned long long result = factorial(n);
+		cout << "Факториал числа " << n << " равен: " << result << "\n";
+	}
+	else {
+		cout << "Ошибка: Введенное число должно быть неотрицательным.\n";
+	}
+
+
+
+	// Запрашиваем у пользователя число для проверки на простое число
+	cout << "Введите число: ";
+	cin >> num;
+
+	if (isPrime(num)) {
+		cout << num << " - простое число.\n";
+	}
+	else {
+		cout << num << " - не является простым числом.\n";
+	}
+
+
+	// Запрашиваем у пользователя число для вычисления куба числа
+	cout << "Введите число: ";
+	cin >> num_cube;
+
+	int result = cube(num_cube);
+
+	cout << "Куб числа " << num_cube << " равен: " << result << "\n";
+
+	return 0;
 }
