a031e708d063c95900a62e87f2f8959bdff2e2da
Igor Valuiskov <i_valuiskov@mail.ru>
2023-07-22 22:21:25 +0400

    решена задача Storehouse

--- Homework/HM6/Storehouse/Storehouse.cpp
+++ Homework/HM6/Storehouse/Storehouse.cpp
@@ -1,31 +1,33 @@
-﻿/*  У швеи имеется ткань длиной L. Ей необходимо пошить подушки длиной P. Ширина ткани совпадает с шириной подушки.
-    Определить сколько подушек сможет пошить швея, если умножать и дельть она не умеет. */
-#include <iostream>
+﻿#include <iostream>
 
 using namespace std;
 
 int main()
 {
-    setlocale(LC_ALL, "Rus");
+	setlocale(LC_ALL, "Rus");
 
-    int L, P;
+	int sum_jashch;
+	int otgruz;
 
-    cout << "Введите длину ткани: ";
-    cin >> L;
+	cout << "Сколько всего ящиков: ";
+	cin >> sum_jashch;
+	int count_car = 1;
+	do
+	{
+		cout << "Приехала " << count_car <<
+			" машина. Сколько ящиков отгружать: ";
+		do
+		{
+			cin >> otgruz;
+			if (otgruz > sum_jashch)
+				cout << "Столько ящиков нет. Укажите меньшее количество: ";
+		} while (otgruz > sum_jashch);
+		sum_jashch -= otgruz;
+		count_car++;
+	} while (sum_jashch > 0);
 
-    cout << "Введите длину подушки: ";
-    cin >> P;
 
-    int count = 0;
+	cout << "Вот столько машин понадобилось чтобы забрать все коробки: " << count_car - 1 <<  endl;
 
-    // Пока длина ткани больше или равна длине подушки, вычитаем длину подушки из длины ткани
-    while (L >= P)
-    {
-        L -= P;
-        count++;
-    }
-
-    cout << "Швея сможет пошить " << count << " подушек.\n";
-
-    return 0;
+	return 0;
 }
