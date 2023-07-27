2740dad6ceea6b80c0fd31a220e5699123cd05c0
Igor Valuiskov <i_valuiskov@mail.ru>
2023-07-27 21:20:21 +0400

    решебник Python две задачки 85 и 86

---
+++ Classwork/ZadachiFromPython/85 Gipotenuza/85 Gipotenuza.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+double gipo(double a, double b)
+{
+	double res = sqrt(pow(a, 2) + pow(b, 2));
+	return res;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+
+	double a, b, c;
+	cout << "Введите длину катетов А и В: ";
+	cin >> a >> b;
+	c = gipo(a, b);
+	cout <<  "Длина гипотенузы равна: " << c << endl;
+
+}
---
+++ Classwork/ZadachiFromPython/86 Taxi/86 Taxi.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+
+	const int base = 4;
+	double km_cost = 0.25;
+
+	cout << "Введите расстояние: ";
+
+
+
+}
