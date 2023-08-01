eb47c86817fb9dbac6e352223133e9b33fc632a6
Igor Valuiskov <i_valuiskov@mail.ru>
2023-08-01 21:20:12 +0400

    доб Lesson 14

---
+++ Classwork/Lesson14/BinarySearch/BinarySearch.cpp
@@ -0,0 +1,75 @@
+﻿#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+template <class T>
+
+void bubbleSort(T a[], long size)
+{
+	long i, j;
+	T x;
+	for (i = 0; i < size; i++)
+	{
+		for (j = size - 1; j > i; j--)
+		{
+			if (a[j - 1] > a[j])
+			{
+				x = a[j - 1];
+				a[j - 1] = a[j];
+				a[j] = x;
+			}
+		}
+	}
+}
+
+int BinarySearch(int A[], int Lb, int Ub, int Key)
+{
+	int M;
+	while (1)
+	{
+		M = (Lb + Ub) / 2;
+		if (Key < A[M])
+		{
+			Ub = M - 1;
+		}
+		else if (Key > A[M])
+		{
+			Lb = M + 1;
+		}
+		else
+		{
+			return M;
+		}
+	if (Lb > Ub)
+		return -1;
+	}
+}
+
+int main()
+{
+	srand(time(NULL));
+	const long SIZE = 10;
+	int ar[SIZE];
+	int key, ind;
+	//до сортировки
+	for (int i = 0; i < SIZE; i++)
+	{
+		ar[i] = rand() % 100;
+		cout << ar[i] << "\t";
+	}
+	cout << "\n\n";
+
+	bubbleSort(ar, 10);
+
+	for (int = 0; i < SIZE; i++)
+	{
+		cout << ar[i] << "\t";
+	
+	}
+
+	cout << "\n\n";
+	
+
+
+}
---
+++ Classwork/Lesson14/Lesson14 Factorial/Lesson14 Factorial.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream> // подключаем cin/cout
+#include <conio.h> // подключаем getch
+
+using namespace std;
+
+int main()
+{
+
+	setlocale(LC_ALL, "Rus");
+	int n;
+	int i;
+	int res;
+	cout << "Введите число: ";
+	cin >> n;
+	res = 1;
+	for (i = 1; i <= n; i++) {
+		res = res * i;
+	}
+	cout << res;
+}
