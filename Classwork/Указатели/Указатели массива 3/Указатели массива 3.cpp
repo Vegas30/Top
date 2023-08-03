191ebeef1bb0337c7e11642dd8f789fd770a25f8
Igor Valuiskov <i_valuiskov@mail.ru>
2023-08-03 21:28:08 +0400

    Lesson 15

---
+++ Classwork/Указатели/Указатели массива 3/Указатели массива 3.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void ShowArray(int arr[], int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		cout << *arr + i) << " ";
+	}
+}
+
+// подсчет суммы элементов массива
+int GetAmount(int* ptr, int size)
+{
+	int sum = 0;
+	for (int i = 0; i < size; i++)
+
+	{
+		sum += *(ptr + i);
+	}
+	return sum;
+}
+
+int main()
+{
+	const int size = 5;
+	int arr[size] = { 33,44,7,8,9 };
+
+
+
+	return 0;
+
+}
