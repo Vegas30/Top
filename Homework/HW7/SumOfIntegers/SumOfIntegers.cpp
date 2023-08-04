a7ae51443843c1daf05ffca14151c16e17e58b2e
Real9 <Real9@DESKTOP-9UUJ87E>
2023-08-04 20:59:39 +0400

    добавлена задачка Сумма Целлых чисел

---
+++ Homework/HW7/SumOfIntegers/SumOfIntegers.cpp
@@ -0,0 +1,23 @@
+﻿/* программа вычисляет сумму целых чисел от a до 500 (значения вводятся с клавиатуры) */
+
+#include <iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+
+    int a;
+    std::cout << "Введите значение a: ";
+    std::cin >> a;
+
+    int sum = 0;
+    for (int i = a; i <= 500; i++) {
+        sum += i;
+    }
+
+    std::cout << "Сумма чисел от " << a << " до 500 равна: " << sum << std::endl;
+
+    return 0;
+
+
+}
