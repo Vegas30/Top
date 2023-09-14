21dff4fb0cf3b4a8cc8933ccadf674f317de96bb
Igor Valuiskov <i_valuiskov@mail.ru>
2023-09-14 21:08:23 +0400

    добавлен урок 17

---
+++ Classwork/Lesson17/Побитовые операции пример/Побитовые операции пример.cpp
@@ -0,0 +1,33 @@
+﻿// Побитовые операции пример.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int y = 2, x = 3, z = 1, k;
+	k = x | y & z; // Операция 1
+	cout << k << endl;
+	k = x | y & ~z; // Операция 2
+	cout << k << endl;
+	k = x ^ y & ~z; // Операция 3
+	cout << k << endl;
+	k = x & y && z; // Операция 4
+	cout << k << endl;
+
+	x = 1;
+	y = -1;
+	k = !x | x; // Операции 5
+	cout << k << endl;
+	k = -x | x; // Операции 6
+	cout << k << endl;
+	k = x ^ x; // Операции 7
+	cout << k << endl;
+	x <<= 3; // Операции 8
+	cout << x << endl;
+	y <<= 3; // Операции 9
+	cout << y << endl;
+	y >>= 3; // Операции 10
+	cout << y << endl;
+	return 0;
+}
