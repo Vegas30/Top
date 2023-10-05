08f859b769da7726ddc46f3eb203ce1c1c599074
Igor Valuiskov <i_valuiskov@mail.ru>
2023-10-05 20:53:18 +0400

    LabWork 05.10.23

---
+++ Classwork/LabWork 05.10.2023/Задание 1 Lift/Задание 1 Lift.cpp
@@ -0,0 +1,41 @@
+﻿// Задание 1 Lift.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+class Lift
+{
+private:
+	int lowerFloor = 1;
+	int upperFloor = 5;
+	bool isOn;
+	bool isPower;
+	int currentFloorNumber;
+	int* floorRange;
+	
+public:
+
+	Lift(const int* floorRange, );
+
+	 
+	void setFloorNumber(int floor)
+	{
+		floorNumber = floor;
+	}
+
+	void setOnOff(int sw)
+	{
+		isOn = sw;
+	}
+
+
+
+};
+
+
+int main()
+{
+
+
+}
