7f738149fa16b3ca63a1dc514e4660124bcb1cb5
RPO_5 <RPO_5@DESKTOP-8OHDLS4>
2023-10-09 21:21:23 +0400

    Неля первый урок

---
+++ C#/Classwork/Labwork 09.10.23/Задание 1 Fuzz Buzz/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Задание_1_Fuzz_Buzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число от 0 до 100: ");
+            int number = 0;
+            number = int.Parse(Console.ReadLine());
+
+            if (number % 3 == 0 & number % 5 == 0)
+            {
+                Console.WriteLine("Fizz Bizz");
+            }
+
+            else if (number % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (number % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else
+            {
+                Console.WriteLine(number);
+            }
+
+
+        }
+    }
+}
