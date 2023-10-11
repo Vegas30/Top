2e3a1b204308f3b46330ada90757b835cfa04eeb
Igor Valuiskov <i_valuiskov@mail.ru>
2023-10-11 16:19:23 +0400

    сделано по 6 задание

---
+++ C#/Classwork/Labwork 09.10.23/Задание 5/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Задание_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите дату: ");
+            string inputDate = Console.ReadLine();
+
+            // Пытаемся преобразовать введенную строку в объект DateTime
+            if (DateTime.TryParseExact(inputDate, "dd.MM.yyyy", null, System.Globalization.DateTimeStyles.None, out DateTime date))
+            {
+                // Получаем название сезона
+                string season = GetSeason(date.Month);
+
+                // Получаем название дня недели
+                DayOfWeek dayOfWeek = date.DayOfWeek;
+                //string dayOfWeek = date.ToString("dddd"); // и так тоже можно
+
+                // Выводим результат
+                Console.WriteLine($"{season} {dayOfWeek}");
+            }
+            else
+            {
+                Console.WriteLine("Некорректный формат даты.");
+            }
+
+            static string GetSeason(int month)
+            {
+                switch (month)
+                {
+                    case 12:
+                    case 1:
+                    case 2:
+                        return "Winter";
+                    case 3:
+                    case 4:
+                    case 5:
+                        return "Spring";
+                    case 6:
+                    case 7:
+                    case 8:
+                        return "Summer";
+                    case 9:
+                    case 10:
+                    case 11:
+                        return "Autumn";
+                    default:
+                        return "Unknown Season";
+                }
+
+            }
+        }
+
+    }
+}
---
+++ C#/Classwork/Labwork 09.10.23/Задание 6/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Задание_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Выберите что переводим:\n1)Цельсий в Фаренгейт\n2)Фаренгейт в Цельсий\n");
+            int choice = 0;
+
+            if (!int.TryParse(Console.ReadLine(), out choice))
+            {
+                Console.WriteLine("Неверный ввод. Пожалуйста, введите корректное число.");
+            }
+
+            Console.WriteLine("Введите температуру: ");
+            double temptemperature = 0;
+
+            if (!double.TryParse(Console.ReadLine(), out temptemperature))
+            {
+                Console.WriteLine("Неверный ввод. Пожалуйста, введите корректное число.");
+            }
+
+
+            switch (choice)
+            {
+                case 1:
+                    //(0 °C × 9/5) + 32
+                    double tempFarengeit = (temptemperature * 9/5) + 32;
+                    Console.WriteLine($"Температура в Фаренгейтах: {tempFarengeit}");
+                    break;
+
+                case 2:
+                    //5/9 (t °F - 32)
+                    double tempCelsius = (temptemperature - 32) * 5/9;
+                    Console.WriteLine($"Температура в Фаренгейтах: {tempCelsius}");
+                    break;
+
+                default:
+                    break;
+            }
+
+
+        }
+    }
+}
