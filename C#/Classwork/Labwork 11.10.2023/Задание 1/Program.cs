37e11eca2c4f84347640eb7f376578a2796650f9
Igor Valuiskov <i_valuiskov@mail.ru>
2023-10-20 20:45:43 +0400

    Исправлени первая задача Модуль2

--- C#/Classwork/Labwork 11.10.2023/Задание 1/Program.cs
+++ C#/Classwork/Labwork 11.10.2023/Задание 1/Program.cs
@@ -59,12 +59,12 @@ namespace Задание_1
                     evenCount++;
                 }
             }
-            for (int i = 0; i < array.Length - 1; i++)
+            for (int i = 0; i < array.Length; i++)
             {
                 int count = 0;
-                for (int j = i + 1; j < array.Length - 1; j++)
+                for (int j = 0; j < array.Length; j++)
                 {
-                    if (array[i] == array[j])
+                    if (i != j && array[i] == array[j])
                     {
                         count++;
                     }
