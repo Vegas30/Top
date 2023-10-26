41df8ba7c4d96ab8065a3918a6b67d175bfe60b2
Igor Valuiskov <i_valuiskov@mail.ru>
2023-10-26 19:53:42 +0400

    Доделано задание ToDo

--- C#/Classwork/Exam/ToDo_List/Program.cs
+++ C#/Classwork/Exam/ToDo_List/Program.cs
@@ -5,14 +5,17 @@
         public string Title;
         public string Description;
         public string Priority;
-        //public enum Priority
-        //{
-        //    Low,
-        //    Middle,
-        //    High
-        //};
-        public DateTime Date;
+        public Priority PriorityLevel;
+        public DateTime? Date;
     }
+
+    enum Priority
+    {
+        низкий,
+        средний,
+        высокий
+    }
+
 
     class Program
     {
@@ -22,12 +25,14 @@
         {
             while (true)
             {
+                int currentLine = Console.CursorTop;
+                Console.SetCursorPosition(Console.CursorLeft, currentLine + 10);
                 Console.WriteLine("Выберите действие:");
                 Console.WriteLine("1. Посмотреть задачи");
                 Console.WriteLine("2. Добавить задачу");
                 Console.WriteLine("3. Удалить задачу");
                 Console.WriteLine("4. Выйти");
-                int choice = 0;
+                int choice;
                 while (!int.TryParse(Console.ReadLine(), out choice))
                 {
                     Console.WriteLine("Ошибка. Введите целое число от 1 до 4.");
@@ -56,29 +61,46 @@
 
         static void ShowTask()
         {
+            Console.Clear();
             foreach (var task in tasks)
             {
                 if (task.Title != null)
-                    Console.WriteLine($"Задача: {task.Title}, Описание: {task.Description} Приоритет: {task.Priority}, Дата: {task.Date} ");
+                    //Console.WriteLine($"Задача: {task.Title}, Описание: {task.Description} Приоритет: {task.PriorityLevel}, Дата: {task.Date} ");
+                    Console.WriteLine("Задача: {0}, \tОписание: {1} \tПриоритет: {2}, \tДата: {3:D} ", task.Title, task.Description, task.PriorityLevel, task.Date);
             }
         }
 
+
         static void AddTask()
         {
-            Console.WriteLine("Введите название задачи:");
-            string title = Convert.ToString(Console.ReadLine());
-            Console.WriteLine("Описание задачи:");
-            string description = Console.ReadLine();
-            Console.WriteLine("Введите приоритет:");
-            string priority = Console.ReadLine();
-            Console.WriteLine("Введите дату:");
-            DateTime date = Convert.ToDateTime(Console.ReadLine());
+            Console.Clear();
 
-            Task newTask = new Task
+            Console.WriteLine("Введите название задачи:");
+            string? title = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine("Описание задачи:");
+            string? description = Console.ReadLine();
+
+            Console.WriteLine("Введите приоритет:");
+            object? priority;
+            while (!Enum.TryParse(typeof(Priority), Console.ReadLine().ToLower(), out priority))
+            {
+                Console.WriteLine("Введите приоритет: Низкий, Средний или Высокий");
+            }
+
+            Console.WriteLine("Введите дату:");
+            DateTime? date = null;
+            string inputDate = Console.ReadLine();
+            if (!string.IsNullOrEmpty(inputDate))
+            {
+                date = DateTime.Parse(inputDate);
+            }
+
+            Task newTask = new()
             {
                 Title = title,
                 Description = description,
-                Priority = priority,
+                PriorityLevel = (Priority)priority,
                 Date = date
             };
 
@@ -98,7 +120,7 @@
         static void RemoveTask()
         {
             Console.WriteLine("Введите имя задачи, которую нужно удалить:");
-            string title = Console.ReadLine();
+            string? title = Console.ReadLine();
 
             for (int i = 0; i < tasks.Length; i++)
             {
---
+++ C#/Classwork/Labwork 11.10.2023/Задание 9 второй способ/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Globalization;
+
+namespace Задание_9_второй_способ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(@"
+    Реализуйте приложение для подсчёта количество вхождений подстроки в строку. Пользователь вводит исходную
+строку и слово для поиска. Приложений отображает результат поиска.
+Например:
+пользователь ввёл: Why she had to go. I don't know, she wouldn't say;
+подстрока для поиска: she;
+результат поиска: 2.
+            " + "\n");
+
+            Console.WriteLine("Введите исходную строку:");
+            string inputString = Console.ReadLine();
+
+            Console.WriteLine("Введите подстроку для поиска:");
+            string searchString = Console.ReadLine();
+
+            int count = 0;
+            string strTemp = "";
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                if (inputString[i] == searchString[0])
+                {
+                    for (int j = 0; j < searchString.Length; j++)
+                    {
+                        if (inputString[i + j] == searchString[j])
+                        {
+                            strTemp = strTemp + inputString[i + j];
+                        }
+                    }
+                    if (strTemp == searchString)
+                    {
+                        count++;
+                    }
+
+                }
+                
+                strTemp = "";
+            }
+
+
+
+
+            Console.WriteLine($"Результат поиска: {count}");
+        }
+    }
+}
