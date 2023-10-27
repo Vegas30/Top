460773975b7cdf776755296007cc6f365522419a
Igor Valuiskov <i_valuiskov@mail.ru>
2023-10-27 21:27:43 +0400

    sdfds

---
+++ C#/Classwork/Read_Write_File/LInq/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+namespace LInq
+{
+
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Ball { get; set; }
+    }
+
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student();
+            student1.Name = "Name1";
+            student1.Ball = 10;
+
+            Student student2 = new Student();
+            student2.Name = "Name2";
+            student2.Ball = 12;
+
+            var list = new List<Student>();
+            list.Add(student1);
+            list.Add(student2);
+
+            var evenSearch = from student in list
+                             where student.Ball >= 11
+                             select student;
+
+            Console.WriteLine("Студенты с оценками больше 11:");
+            foreach (var stu in evenSearch)
+            {
+                Console.WriteLine(stu);
+            }
+
+        }
+    }
+}
---
+++ C#/Classwork/Read_Write_File/Read_Write_File/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Read_Write_File
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите текст: ");
+            string inputText = Console.ReadLine();
+            string path = @"D:\example.csv";
+            File.AppendAllText(path, inputText + Environment.NewLine);
+
+
+            string content = File.ReadAllText(path);
+            Console.WriteLine("Содержимое файла: " + content);
+            string[] words = content.Split(' ');
+            int count=0;
+            foreach (var word in words)
+            {
+                count++;
+            }
+
+            Console.WriteLine(count);
+            
+
+
+            Console.ReadKey();
+        }
+    }
+}
