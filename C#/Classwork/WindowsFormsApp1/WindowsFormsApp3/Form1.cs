e876846f14d915d39a741bcf8afa03ae3d21376e
Igor Valuiskov <i_valuiskov@mail.ru>
2023-10-30 21:20:21 +0400

    WinForm первый урок

---
+++ C#/Classwork/WindowsFormsApp1/WindowsFormsApp3/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    class Raschet
+    {
+        int num1;
+        int num2;
+
+        public int Add()
+        {
+            return num1 + num2;
+        }
+    }
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
