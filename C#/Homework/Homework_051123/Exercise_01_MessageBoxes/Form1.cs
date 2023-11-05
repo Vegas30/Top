72621c753095077f981c269f395ccd1ec333b39d
Igor Core2Duo <i_valuiskov@mail.ru>
2023-11-05 14:56:41 +0400

    задачка к Лабораторной Message Boxes

---
+++ C#/Homework/Homework_051123/Exercise_01_MessageBoxes/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise_01_MessageBoxes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string resume = "Игорь Валуйсков\r\nВеб-разработчик\r\ni_valuiskov@mail.ru github.com/Vegas30\r\n\r\nЯ учусь веб-разработке. Решил стать front-end разработчиком. Это мои первые шаги в изучении основ HTML и CSS. Изучал професию тестировщика, даже прошел платный курс на одном известном обучающем портале. Не понравилось, ни обучение ни сама професия, видимо не моё или преподавание было слабое. Чуствую что разработка мне по душе, прохожу обучение на Хекслете с удовольствием.\r\n\r\nГотов к удаленной работе. Готов обучаться дальше.\r\n\r\nОпыт работы\r\nНастроение ООО Россия\r\nДиректор(учередитель), апрель 2004 - ноябрь 2010\r\nОткрытие кофейни\r\nОрганизация работы\r\nМобител ООО Россия\r\nДиректор(учредитель), апрель 2003 - ноябрь 2008\r\nОфициальный дилер Мегафон\r\nОрганизация работы сети салонов сотовой связи\r\nОбразование\r\nАГУ г.Астрахани\r\nФиз.мат информатика Бакалавр 1996 - 2000\r\nОткрытый университет\r\nMBA 2009\r\nДругие навыки и увлечения\r\nОпытный пользователь ПК\r\nЗнание английского языка\r\nИгорь Валуйсков\r\n\r\nг.Астрахань, ул. Московская, +7977122222, i_valuiskov@mail.ru.";
+            int totalCharacters = resume.Length;
+            int numMessageBoxes = 3;
+            int averageCharactersPerPage = totalCharacters / numMessageBoxes;
+
+            MessageBox.Show(resume.Substring(0, averageCharactersPerPage), "Резюме (1/3)");
+
+            int remainingCharacters = totalCharacters - averageCharactersPerPage;
+            int length = Math.Min(averageCharactersPerPage, remainingCharacters);
+            MessageBox.Show(resume.Substring(averageCharactersPerPage, length), "Резюме (2/3)");
+
+            remainingCharacters -= length;
+            MessageBox.Show(resume.Substring(averageCharactersPerPage * 2, remainingCharacters), "Резюме (3/3)" + $"Среднее количество символов на странице: {averageCharactersPerPage}", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            //MessageBox.Show($"Среднее количество символов на странице: {averageCharactersPerPage}", "Среднее количество символов", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            Application.Exit();
+        }
+    }
+}
---
+++ C#/Homework/Homework_051123/Exercise_01_MessageBoxes/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("Exercise_01_MessageBoxes")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Exercise_01_MessageBoxes")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("d405a720-8101-44f8-b534-dfd5b0f64d4d")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
