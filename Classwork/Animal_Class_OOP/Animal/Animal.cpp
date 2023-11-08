2d5f6f40059160597a751172dc90940c9e78af6b
Igor Valuiskov <i_valuiskov@mail.ru>
2023-11-08 21:28:12 +0400

    классы C++

---
+++ Classwork/Animal_Class_OOP/Animal/Animal.cpp
@@ -0,0 +1,50 @@
+﻿// Animal.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+
+class Animal {
+private:
+    std::string name;
+
+public:
+    Animal(const std::string name) : name(name) {}
+
+    virtual void makeSound() const {
+        //std::cout << "Some sound from an animal" << std::endl;
+    }
+
+    void printName() {
+        std::cout << "This is " << name << std::endl;
+    }
+};
+
+class Cat : Animal {
+public:
+    Cat(const std::string name) : Animal(name) {}
+
+    void makeSound() const override {
+        std::cout << "meow" << std::endl;
+    }
+};
+
+class Dog : Animal {
+public:
+    Dog(const std::string name) : Animal(name) {}
+
+    void makeSound() const override {
+        std::cout << "woof" << std::endl;
+    }
+};
+
+int main()
+{
+    Cat cat("Cat");
+    Dog dog("Buddy");
+
+    cat.makeSound();
+    dog.makeSound();
+}
+
+
