85a22a43d94faa1bff3a66268b850b5eabf7bd8a
Igor Valuiskov <i_valuiskov@mail.ru>
2023-11-15 23:46:13 +0400

    добавлен Класс Animal и Employee с урока

--- Classwork/Animal_Class_OOP/Animal/Animal.cpp
+++ Classwork/Animal_Class_OOP/Animal/Animal.cpp
@@ -2,61 +2,113 @@
 //
 
 #include <iostream>
-
+#include <string>
 
 class Animal {
-private:
+protected:
     std::string name;
-    std::string food;
+    int age;
+    double weight;
 
 public:
-    Animal(const std::string name, const std::string food) : name(name), food(food) {}
+    Animal(const std::string name, int age, double weight) : name(name), age(age), weight(weight) {}
 
-    virtual void makeSound() const {
-       // std::cout << "Some sound from an animal" << std::endl;
+    virtual void makeSound() const = 0;
+
+    virtual void displayInfo() const {
+        std::cout << "Животное: " << name << ", Возраст: " << age << " лет, Вес: " << weight << " кг" << std::endl;
     }
 
-    void printName() {
-        std::cout << "This is " << name << std::endl;
-    }
+    virtual double calculateVaccineDose() const = 0; 
 };
 
-class Cat : Animal {
+class Dog : public Animal {
+private:
+    std::string breed;
+    double vaccineDose = 0.05;
+
 public:
-    Cat(const std::string name, const std::string food) : Animal(name, food) {}
+    Dog(const std::string name, int age, double weight, const std::string breed) : Animal(name, age, weight), breed(breed) {}
 
     void makeSound() const override {
-        std::cout << "meow" << std::endl;
+        std::cout << "Гав-гав!" << std::endl;
+    }
+
+    void displayInfo() const override {
+        Animal::displayInfo();
+        std::cout << "Порода: " << breed << std::endl;
+    }
+
+    double calculateVaccineDose() const override {
+        return weight * vaccineDose;
     }
 };
 
-class Dog : Animal {
+class Cat : public Animal {
+private:
+    bool hasTail;
+    double vaccineDose = 0.03;
+
 public:
-    Dog(const std::string name, const std::string food) : Animal(name, food) {}
+    Cat(const std::string name, int age, double weight, bool hasTail) : Animal(name, age, weight), hasTail(hasTail) {}
 
     void makeSound() const override {
-        std::cout << "woof" << std::endl;
+        std::cout << "Мяу!" << std::endl;
+    }
+
+    void displayInfo() const override {
+        Animal::displayInfo();
+        std::cout << "Хвост: " << (hasTail ? "есть" : "отсутствует") << std::endl;
+    }
+
+    double calculateVaccineDose() const override {
+        return weight * vaccineDose;
     }
 };
 
-class Bird : Animal {
+class Bird : public Animal {
+private:
+    std::string species;
+    double vaccineDose = 0.03;
+
 public:
-    Bird(const std::string name, const std::string food) : Animal(name, food) {}
+    Bird(const std::string name, int age, double weight, const std::string species) : Animal(name, age, weight), species(species) {}
 
     void makeSound() const override {
-        std::cout << "Twit-twit" << std::endl;
+        std::cout << "Чирик-чирик!" << std::endl;
+    }
+
+    void displayInfo() const override {
+        Animal::displayInfo();
+        std::cout << "Вид: " << species << std::endl;
+    }
+
+    double calculateVaccineDose() const override {
+        return weight * vaccineDose;
     }
 };
 
-int main()
-{
-    Cat cat("Cat","Fish");
-    Dog dog("Buddy","Meat");
-    Bird bird("Kanareik","Seed");
+int main() {
+    setlocale(LC_ALL, "ru");
 
-    cat.makeSound();
+    Dog dog("Барон", 3, 15.0, "Лабрадор");
+    Cat cat("Мурка", 5, 5.0, true);
+    Bird bird("Чижик", 2, 0.5, "Воробей");
+
+    std::cout << "Информация о животных:" << std::endl;
+    std::cout << "-------------------------" << std::endl;
+    dog.displayInfo();
+    std::cout << "Доза вакцины: " << dog.calculateVaccineDose() << " мл" << std::endl;
     dog.makeSound();
+    std::cout << "-------------------------" << std::endl;
+    cat.displayInfo();
+    std::cout << "Доза вакцины: " << cat.calculateVaccineDose() << " мл" << std::endl;
+    cat.makeSound();
+    std::cout << "-------------------------" << std::endl;
+    bird.displayInfo();
+    std::cout << "Доза вакцины: " << bird.calculateVaccineDose() << " мл" << std::endl;
     bird.makeSound();
+    std::cout << "-------------------------" << std::endl;
+
+    return 0;
 }
-
-
---
+++ Classwork/Animal_Class_OOP/Animal_void/Animal_void.cpp
@@ -0,0 +1,69 @@
+﻿// Animal_void.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+
+class Animal {
+protected:
+	std::string name;
+	int age;
+	double weight;
+	double vaccineDose;
+
+public:
+	Animal(const std::string name, int age, double weight) : name(name), age(age), weight(weight), vaccineDose(0.0) {}
+
+	virtual void makeSound() const = 0;
+
+	virtual void displayInfo() const {
+		std::cout << "Животное: " << name << ", Возраст: " << age << " лет, Вес: " << weight << " кг" << std::endl;
+	}
+
+	virtual void calculateVaccineDose(double dose) = 0;
+
+	double getVaccineDose() const {
+		return vaccineDose;
+	}
+
+};
+
+class Dog : public Animal {
+private:
+	std::string breed;
+
+
+public:
+	Dog(const std::string name, int age, double weight, const std::string breed) : Animal(name, age, weight), breed(breed) {}
+
+	void makeSound() const override {
+		std::cout << "Гав-гав!" << std::endl;
+	}
+
+	void displayInfo() const override {
+		Animal::displayInfo();
+		std::cout << "Порода: " << breed << std::endl;
+	}
+
+	void calculateVaccineDose(double dose) override {
+		vaccineDose = weight * dose;
+	}
+
+
+};
+
+int main() {
+	setlocale(LC_ALL, "ru");
+
+	Dog dog("Барон", 3, 15.0, "Лабрадор");
+
+	std::cout << "Информация о собаке:" << std::endl;
+	std::cout << "-------------------------" << std::endl;
+	dog.displayInfo();
+	dog.calculateVaccineDose(0.05);
+	std::cout << "Доза вакцины: " << dog.getVaccineDose() << " мл" << std::endl;
+	dog.makeSound();
+	std::cout << "-------------------------" << std::endl;
+
+	return 0;
+}
